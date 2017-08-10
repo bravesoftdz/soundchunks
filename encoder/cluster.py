efe011a104af497f753cf8a7754aa9df9381615a
GliGli <sfx.gligli@free.fr>
2017-08-10 11:18:20 +0200

    cluster.py: back to kmeans, works overall best

--- encoder/cluster.py
+++ encoder/cluster.py
@@ -15,9 +15,9 @@ data = np.delete(data, 0, 1)
 
 #data = preprocessing.normalize(data, norm = 'max', axis = 0);
 
-#clus = cluster.KMeans(n_clusters = args.n, verbose = args.d, tol = args.t, random_state = 42, precompute_distances = True, copy_x = False, algorithm = 'full')
+clus = cluster.KMeans(n_clusters = args.n, verbose = args.d, tol = args.t, random_state = 42, precompute_distances = True, copy_x = False, algorithm = 'full')
 #clus = cluster.AgglomerativeClustering(n_clusters = args.n)
-clus = cluster.SpectralClustering(n_clusters = args.n, assign_labels = 'discretize', gamma = 0.15, random_state = 42, eigen_tol = args.t)
+#clus = cluster.SpectralClustering(n_clusters = args.n, assign_labels = 'discretize', gamma = 0.15, random_state = 42, eigen_tol = args.t)
 
 clus.fit(data)
 
